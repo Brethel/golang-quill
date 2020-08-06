e022a1565ddbffea272e7a60f79f5721de5210a4
Alexandre Brethel <brethel@gmail.com>
2020-08-06 09:30:13 -0300

    how does GO111MODULES works ?

--- src/quill/quill.go
+++ src/quill/quill.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
+/**
+ * first GOlang experiments
+ */
 func main() {
-	fmt.Printf("hello world !\n\n")
-}
+	var myFunc = func(a, b int) int {
+		return a + b
+	}
+	fmt.Printf("hello world %d!\n\n", myFunc(1, 2))
 
+	var array = []int{11, 22, 33, 44}
+	for idx, val := range array {
+		fmt.Println(idx, val)
+	}
+
+	var s = [][]byte{[]byte("hello"), []byte("world")}
+
+	fmt.Printf("%s\n", bytes.Join(s, []byte(", ")))
+
+}
